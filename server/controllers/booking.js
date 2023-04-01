e3d44cd667b7304af915937bae3c04e6a7a940e7
nityam01 <nityam0213@gmail.com>
2023-04-01 09:09:08 +0530

    feat: make boooking route working

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -10,6 +10,63 @@ const getAllBookings = async (req, res) => {
   }
 }
 
+const getBookingById = async (req, res) => {
+  let bookings
+  try {
+    bookings = await booking.findById(req.params.id)
+    res.status(200).json(bookings)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const postBooking = async (req, res) => {
+  let bookings
+  try {
+    bookings = await booking.create(req.body)
+    res.status(200).json(bookings)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const updateBooking = async (req, res) => {
+  const { email, roomNumber, roomType, startDate, endDate } = req.body
+  if (!email || !roomNumber || !roomType || !startDate || !endDate) {
+    res.status(400).json({ message: 'Please provide all required fields' })
+  }
+
+  try {
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      req.params.id,
+      {
+        email,
+        roomNumber,
+        roomType,
+        startDate,
+        endDate,
+      },
+      { new: true }
+    )
+    res.status(200).json(updatedBooking)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
+const deleteBooking = async (req, res) => {
+  try {
+    await User.findByIdAndRemove(req.params.id)
+    res.json({ message: 'User deleted' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
 module.exports = {
   getAllBookings,
+  postBooking,
+  getBookingById,
+  updateBooking,
+  deleteBooking,
 }
--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,8 +1,18 @@
 const express = require('express')
-const { getAllBookings } = require('../controllers/booking')
+const {
+  getAllBookings,
+  getBookingById,
+  postBooking,
+  updateBooking,
+  deleteBooking,
+} = require('../controllers/booking')
 
 const bookingRouter = express.Router()
 
 bookingRouter.get('/', getAllBookings)
+bookingRouter.get('/:id', getBookingById)
+bookingRouter.post('/', postBooking)
+bookingRouter.put('/:id', updateBooking)
+bookingRouter.delete('/:id', deleteBooking)
 
 module.exports = bookingRouter
